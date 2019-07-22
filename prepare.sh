ecc864105030a728480e7333c479588d2c532bd4
Cynicsss <cynicsss233@gmail.com>
2019-07-22 16:34:30 +0800

    init

---
+++ prepare.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+python3 setup.py develop
+pip3 install pycocotools
+pip3 install terminaltables
+pip3 install mmcv
+mkdir data
+cd data
+hdfs dfs -get $PAI_DEFAULT_FS_URI/data/datasets/undertotalcoco.zip
+unzip -q undertotalcoco.zip
+mv ./coco/annotations/instance_train2017.json ./coco/annotations/instances_train2017.json
+mv ./coco/annotations/instance_val1.json ./coco/annotations/instances_val2017.json
+mv ./coco/val12017 ./coco/val2017
+cd ..
+bash tools/dist_train.sh configs/faster_rcnn_r101_fpn_1x.py 2
