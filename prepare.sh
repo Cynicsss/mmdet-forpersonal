d4c3b906d4ab1e9222b72222245eb49c0e237bd0
Cynicsss <cynicsss233@gmail.com>
2019-07-28 14:03:36 +0800

    add kmeans

--- prepare.sh
+++ prepare.sh
@@ -10,4 +10,6 @@ cd data
 hdfs dfs -get $PAI_DEFAULT_FS_URI/data/datasets/2018origincoco.zip
 unzip -q 2018origincoco.zip
 cd ..
+hdfs dfs -get $PAI_DEFAULT_FS_URI/data/models/zhangyu/resnext101_64x4d-ee2c6f71.pth
+mv ./resnext101_64x4d-ee2c6f71.pth /root/.cache/torch/checkpoints/
 bash tools/dist_train.sh configs/faster_rcnn_x101_64x4d_fpn_1x.py 2
